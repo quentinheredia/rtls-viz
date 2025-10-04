567d8904677c921174db8417c2f3947832c0d604
quentinheredia <quentin.heredia04@gmail.com>
2025-10-04 19:09:24 -0400

    added basename to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/rtls-viz">
         <Routes>
           <Route element={<AppLayout />}>
             <Route path="/" element={<Overview />} />
